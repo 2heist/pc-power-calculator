d52aba7792e9dba205c421bda75bfe0f769b1c91
Edward <edwardkawa907@gmail.com>
2025-05-21 16:48:25 +0300

    Added static file handling with error management

--- server.js
+++ server.js
@@ -51,7 +51,36 @@ const server = http.createServer((req, res) => {
 
   const filePath = path.join(__dirname, pathname.startsWith('/public') ? '.' : './public', pathname);
   const extname = path.extname(filePath);
-});
+
+  fs.readFile(filePath, (err, content) => {
+    if(err) {
+      if(err.code === 'ENOENT') {
+        fs.readFile(path.join(__dirname, 'public', '404.html' ), (err, content) => {
+
+          if (err) {
+          res.writeHead(404, { 'Content-Type': 'text/plain' });
+          res.end('404 Not Found');
+          } else {
+          res.writeHead(404, { 'Content-Type': 'text/html' });
+          res.end(content, 'utf8');
+          } 
+
+        });
+
+      } else {
+
+          res.writeHead(500, { 'Content-Type': 'text/plain' });
+          res.end(`Server Error: ${err.code}`);
+
+        }
+
+    } else {
+
+        res.writeHead(200, {'Content-type': contentTypes[extname] || 'text/plain' });
+        res.end(content, 'utf8');
+      }
+  });
+
 
 const PORT = process.env.PORT || 3000; 
 server.listen(PORT, () => {
