e1c0dae5ad36bf135653b2de6678aea0bea85ff0
Edward <edwardkawa907@gmail.com>
2025-05-22 11:07:06 +0300

    Created power calculation function

--- server.js
+++ server.js
@@ -141,6 +141,49 @@ if (components.cooler) {
   
 }
 
+function calculateTotalPower(components) {
+
+  let totalPower = 0;
+
+  if (components.cpu) {
+
+    const cpuComponent = componentsData.cpu.find(c => c.name === components.cpu);
+    if (cpuComponent) totalPower += cpuComponent.power
+
+  }
+
+  if (components.gpu) {
+    const gpuComponent = componentsData.gpu.find(g => g.name === components.gpu);
+    if (gpuComponent) totalPower += gpuComponent.power;
+  }
+
+   if (components.ram && Array.isArray(components.ram)) {
+    components.ram.forEach(ram => {
+      const ramComponent = componentsData.ram.find(r => r.size === ram);
+      if (ramComponent) totalPower += ramComponent.power;
+    });
+  }
+
+   if (components.storage && Array.isArray(components.storage)) {
+    components.storage.forEach(storage => {
+      const storageComponent = componentsData.storage.find(s => s.size === storage);
+      if (storageComponent) totalPower += storageComponent.power;
+    });
+  }
+
+  if (components.cooler && Array.isArray(components.cooler)) {
+    components.cooler.forEach(cooler => {
+      const coolerComponent = componentsData.cooler.find(c => c.size === cooler);
+      if (coolerComponent) totalPower += coolerComponent.power;
+    });
+  }
+
+  totalPower = Math.round(totalPower * 1.3);
+  
+return totalPower;
+}
+
+
 const PORT = process.env.PORT || 3000; 
 server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
